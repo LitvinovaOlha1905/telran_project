a28a8bb817edc656845c1fb12b6cc1d7132822a0
IKaschkarov <142896713+IKaschkarov@users.noreply.github.com>
2024-04-26 20:30:24 +0200

    Added file Footer, styles

--- project_garden/src/components/Footer/Footer.jsx
+++ project_garden/src/components/Footer/Footer.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import styles from './Footer.module.css'
+import { IoLogoWhatsapp } from "react-icons/io";
+import { RiInstagramFill } from "react-icons/ri";
 
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <h2>Contact</h2>
+      <div className={styles.info_container}>
+
+        <div>
+          <h6>Phone</h6>
+          <a href="tel:+499999999999">
+            +49 999 999 99 99
+          </a>
+        </div>
+
+        <div>
+          <h6>Socials</h6>
+          
+          <div>
+          <RiInstagramFill />
+          <IoLogoWhatsapp />
+          </div>
+        </div>
+
+        <div>
+          <h6>Address</h6>
+          <p>Linksraße 2, 8 OG, 10785, Berlin, Deutschland</p>
+        </div>
+
+        <div>
+          <h6>Working Hours</h6>
+          <p>24 hours a day</p>
+        </div>
+      </div>
+      
+      <div>
+        Карта
+      </div>
+       
+
+    </div>
   )
 }
