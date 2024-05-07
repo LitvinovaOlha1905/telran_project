d7e96026c342f7b159a57626954b034601975f22
IKaschkarov <142896713+IKaschkarov@users.noreply.github.com>
2024-05-07 11:12:26 +0200

    update categoriesSlice.jsx, CategoriesContainer.jsx, CategoryCard.jsx + styles

--- project_garden/src/pages/CategoriesPage/CategoriesPage.jsx
+++ project_garden/src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import CategoriesContainer from '../../components/Categories/CategoriesContainer';
+
 
 
 export default function CategoriesPage() {
   return (
     <div>
-        <CategoriesContainer />
+        All Categories
     </div>
   );
 }
--- project_garden/src/pages/MainPage/MainPage.jsx
+++ project_garden/src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
+import CategoriesContainer from "../../components/Categories/CategoriesContainer";
 
 export default function MainPage() {
 	return (
 		<div>
 			<Banner />
+			<CategoriesContainer/>
 		</div>
 	);
 }
