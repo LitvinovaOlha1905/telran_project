d7f04e46eb52ec5c7bcfe99908bba2634923a3aa
olena-star <122024787+olena-star@users.noreply.github.com>
2024-05-10 17:19:50 +0200

    Create Skeleton

--- project_garden/src/components/GetDiscount/GetDiscount.jsx
+++ project_garden/src/components/GetDiscount/GetDiscount.jsx
@@ -1,23 +1,23 @@
+import React from 'react';
 import DiscountForm from '../DiscountForm/DiscountForm';
-import image from '../../images/DiscountForm/image_form.png'
-import styles from '../GetDiscount/GetDiscount.module.css'
+import image from '../../images/DiscountForm/image_form.png';
+import styles from './GetDiscount.module.css'; 
 
 const GetDiscount = () => {
   return (
     <section className={styles.wrapper}>
-      <div className='container'>
-      <div className={styles.container}>
+      <div className={styles.containerForm}>
+        <div className={styles.container}>
           <h2 className={styles.title}>
-            <p>5% off on the first order</p>
+            5% off on the first order
           </h2>
-        <article className={styles.container_form}>
-        <img className={styles.image} src={image} alt="Image" />
-          <DiscountForm/>
-        </article>
-      </div>
+          <article className={styles.container_form}>
+            <img className={styles.image} src={image} alt="Image" />
+            <DiscountForm/>
+          </article>
+        </div>
       </div>
     </section>
-    
   );
 };
 
