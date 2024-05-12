ec876aab6b3f0d09458aef5f518ef5d0ed767c53
Olha Litvinova <130159769+LitvinovaOlha1905@users.noreply.github.com>
2024-05-12 15:02:33 +0200

    Create AllSalesPage.jsx and AllSalesPage.module.css

--- project_garden/src/pages/AllSalesPage/AllSalesPage.jsx
+++ project_garden/src/pages/AllSalesPage/AllSalesPage.jsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from "react"
+import ProductCard from "../../components/ProductCard/ProductCard"
+import styles from "./AllSalesPage.module.css"
+import { useSelector } from "react-redux"
 
 export default function AllSalesPage() {
-  return (
-    <div>AllSalesPage</div>
-  )
+	const { products, status } = useSelector(state => state.products)
+	const discountedProducts = products?.filter(product => product.discont_price)
+
+	if (status === "loading") {
+		return <h1>Loading...</h1>
+	}
+	return (
+		<div className={styles.wrapper}>
+			<div className='container'>
+				<h1 className={styles.title}>Discounted items</h1>
+				{/* Price */}
+				<div className={styles.sortBlock}>
+					<form className={styles.formSortBlock}>
+						<label className={styles.sortLabel}>Price</label>
+						<input
+							className={[styles.input, styles.sortPriceInput].join(" ")}
+							type='number'
+							pattern='[0-9]*'
+							placeholder='from'
+						/>
+						<input
+							className={[styles.input, styles.sortPriceInput].join(" ")}
+							type='number'
+							pattern='[0-9]*'
+							placeholder='to'
+						/>
+					</form>
+
+					{/* Discounted items */}
+					<form className={styles.formSortBlock}>
+						<label className={styles.sortLabel}>Discounted items</label>
+						<input
+							className={[styles.input, styles.sortDiscountInput].join(" ")}
+							type='checkbox'
+						/>
+					</form>
+
+					{/* Select - уточнить у Нелли как стилизрвать option */}
+					<form className={styles.formSortBlock}>
+						<label className={styles.sortLabel} htmlFor='sortSelect'>
+							Sorted
+						</label>
+						<select className={styles.sortSelect}>
+							<option value='default'>by default</option>
+							<option value='priceAsc'>low to High</option>
+							<option value='priceDesc'>high to Low</option>
+							<option value='discounted'>discounted items</option>
+						</select>
+					</form>
+				</div>
+
+				{/* Cards */}
+				<div className={styles.productsBlock}>
+					{discountedProducts.map(el => (
+						<ProductCard key={el.id} {...el} />
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
