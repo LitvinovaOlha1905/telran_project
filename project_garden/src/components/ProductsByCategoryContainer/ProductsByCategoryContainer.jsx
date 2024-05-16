5b2884d65543918b61dccfa35703fd75afd6745a
IKaschkarov <142896713+IKaschkarov@users.noreply.github.com>
2024-05-16 09:28:02 +0200

    Uptd ProductsByCategoryContainer.jsx, CategoriesPage.jsx added Sceleton

--- project_garden/src/components/ProductsByCategoryContainer/ProductsByCategoryContainer.jsx
+++ project_garden/src/components/ProductsByCategoryContainer/ProductsByCategoryContainer.jsx
@@ -19,10 +19,12 @@ export default function ProductsByCategoryContainer() {
       dispatch(fetchProductsByCategory(categoryId));
     }, [dispatch, categoryId]);
 
-  
+  console.log(currentCategory);
   return (
     <div>
-      <h1>{currentCategory.title}</h1>
+      {currentCategory && (
+        <h1 className={styles.title}>{currentCategory.title}</h1>
+      )}
       {status === "loading" ? (
         <p>Loading...</p>
       ) : (
