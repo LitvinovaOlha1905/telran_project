d53a3daa7db5a98699f9d4084f1db841dfb7c4dc
IKaschkarov <142896713+IKaschkarov@users.noreply.github.com>
2024-05-19 13:06:05 +0200

    Updt styles CategoriesCard

--- project_garden/src/components/ProductByCategoryCard/ProductByCategoryCard.jsx
+++ project_garden/src/components/ProductByCategoryCard/ProductByCategoryCard.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import styles from './ProductByCategoryCard.module.css'
+import { Link } from 'react-router-dom';
 
 export default function ProductByCategoryCard({ id, image, title, price, discont_price }) {
     return (
       <div className={styles.cardBlock}>
-        <img src={`http://localhost:3333${image}`} alt={title} />
+        <Link to={`/product/${id}`}>
+          <img src={`http://localhost:3333${image}`} alt={title} />
 
-        <div className={styles.descriptionBlock}>
-          <p>{title}</p>
-          <div className={styles.priceBlock}>
-            <p>
-              {"\u0024"}
-              {price}
-            </p>
-            {discont_price ? (
-              <span>
+          <div className={styles.descriptionBlock}>
+            <p>{title}</p>
+            <div className={styles.priceBlock}>
+              <p>
                 {"\u0024"}
-                {discont_price}
-              </span>
-            ) : null}
+                {price}
+              </p>
+              {discont_price ? (
+                <span>
+                  {"\u0024"}
+                  {discont_price}
+                </span>
+              ) : null}
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
 }
