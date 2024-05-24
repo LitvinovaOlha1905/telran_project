77c77d9c0c84a08ca23056afd2f48c14573278e7
Olha Litvinova <130159769+LitvinovaOlha1905@users.noreply.github.com>
2024-05-24 16:16:57 +0200

    update CustomSelect and FromSelect

--- project_garden/src/pages/AllSalesPage/AllSalesPage.jsx
+++ project_garden/src/pages/AllSalesPage/AllSalesPage.jsx
@@ -3,10 +3,14 @@ import Sceleton from "../../components/Sceleton/Skeleton"
 import ProductCard from "../../components/ProductCard/ProductCard"
 import styles from "./AllSalesPage.module.css"
 import { useSelector } from "react-redux"
+import FormSortPrice from "../../components/FormSort/FormSortPrice/FormSortPrice"
+import FormSelect from "../../components/FormSort/FormSelect/FormSelect"
 
 export default function AllSalesPage() {
 	const { products } = useSelector(state => state.products)
 	const discountedProducts = products?.filter(product => product.discont_price)
+
+	console.log(discountedProducts);
 
 	useEffect(() => {
 		window.scrollTo(0, 0)
@@ -25,59 +29,24 @@ export default function AllSalesPage() {
 		<div className={styles.wrapper}>
 			<div className='container'>
 				<h1 className={styles.title}>Discounted items</h1>
+
 				{/* Price */}
-				<div className={styles.sortBlock}>
-					<form className={styles.formSortBlock}>
-						<label className={styles.sortLabel}>Price</label>
-						<input
-							className={[styles.input, styles.sortPriceInput].join(" ")}
-							type='number'
-							pattern='[0-9]*'
-							placeholder='from'
-						/>
-						<input
-							className={[styles.input, styles.sortPriceInput].join(" ")}
-							type='number'
-							pattern='[0-9]*'
-							placeholder='to'
-						/>
-					</form>
+				<FormSortPrice />
 
-					{/* Discounted items */}
-					<form className={styles.formSortBlock}>
-						<label className={styles.sortLabel}>Discounted items</label>
-						<input
-							className={[styles.input, styles.sortDiscountInput].join(" ")}
-							type='checkbox'
-						/>
-					</form>
-
-					{/* Select - уточнить у Нелли как стилизрвать option */}
-					<form className={styles.formSortBlock}>
-						<label className={styles.sortLabel} htmlFor='sortSelect'>
-							Sorted
-						</label>
-						<select className={styles.sortSelect}>
-							<option value='default'>by default</option>
-							<option value='priceAsc'>low to High</option>
-							<option value='priceDesc'>high to Low</option>
-							<option value='discounted'>discounted items</option>
-						</select>
-					</form>
-				</div>
+				{/* Select */}
+				<FormSelect />
 
 				{/* Cards */}
-
 				{isLoading ? (
 					<Sceleton />
 				) : (
 					<div className={styles.productsBlock}>
-						{discountedProducts.map(el => (
-							<ProductCard key={el.id} {...el} />
-						))}
+						{discountedProducts.map(
+							el => el.visible && <ProductCard key={el.id} {...el} />
+						)}
 					</div>
 				)}
 			</div>
 		</div>
-	)
+	);
 }
