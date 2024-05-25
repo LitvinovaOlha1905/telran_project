6e2e11e001563a06fbab705cc67fa72cafe732ff
Olha Litvinova <130159769+LitvinovaOlha1905@users.noreply.github.com>
2024-05-25 19:19:40 +0200

    update ProductsCard.jsx and ProductsCard.module.css

--- project_garden/src/components/ProductCard/ProductCard.jsx
+++ project_garden/src/components/ProductCard/ProductCard.jsx
@@ -6,7 +6,8 @@ import { ReactComponent as IconHeart } from "../../images/Icons/heart.svg";
 import { ReactComponent as IconHertActive } from "../../images/Icons/heartActive.svg";
 import { ReactComponent as IconBagActive } from "../../images/Icons/bagActive.svg";
 
-export default function ProductCard({id,
+export default function ProductCard({
+	id,
 	image,
 	title,
 	price,
@@ -23,9 +24,7 @@ export default function ProductCard({id,
 
 					{/* Description Block */}
 					<div className={styles.descriptionBlock}>
-						<p className={styles.description}>
-							{title.length > 20 ? `${title.substring(0, 17)}...` : title}
-						</p>
+						<p className={styles.description}>{title}</p>
 						<div className={styles.priceBlock}>
 							<p className={styles.price}>
 								{"\u0024"}
