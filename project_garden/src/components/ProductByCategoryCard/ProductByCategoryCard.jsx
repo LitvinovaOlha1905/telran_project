e25ba71142a994aeac4f64224e319a8b2d3fb8e2
Olha Litvinova <130159769+LitvinovaOlha1905@users.noreply.github.com>
2024-06-07 11:02:59 +0200

    added adaptive ProductCard, ProductContainer, CategoriCard, CategoryContainer, etc

--- project_garden/src/components/ProductByCategoryCard/ProductByCategoryCard.jsx
+++ project_garden/src/components/ProductByCategoryCard/ProductByCategoryCard.jsx
@@ -5,89 +5,94 @@ import { ReactComponent as IconBag } from "../../images/Icons/bag.svg";
 import { ReactComponent as IconHeart } from "../../images/Icons/heart.svg";
 import { ReactComponent as IconHertActive } from "../../images/Icons/heartActive.svg";
 import { ReactComponent as IconBagActive } from "../../images/Icons/bagActive.svg";
-import { addProduct, countTotalSum } from '../../store/slices/cartSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { addProduct, countTotalSum } from "../../store/slices/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
+export default function ProductByCategoryCard({
+	id,
+	image,
+	title,
+	price,
+	discont_price,
+}) {
+	const [heartActive, setHeartActive] = useState(false);
+	const [bagActive, setBagActive] = useState(false);
 
-export default function ProductByCategoryCard({ id, image, title, price, discont_price }) {
-   
+	const { productsInCart } = useSelector(store => store.cart);
 
-  const [heartActive, setHeartActive] = useState(false);
-  const [bagActive, setBagActive] = useState(false);
+	const dispatch = useDispatch();
+	const product = {
+		id: id,
+		title: title,
+		image: image,
+		price: price,
+		discont_price: discont_price,
+	};
+	const handleAddToCart = event => {
+		// event.preventDefault();
+		console.log(product);
+		dispatch(addProduct(product));
+		dispatch(countTotalSum());
+		console.log(productsInCart);
+	};
 
-  const { productsInCart } = useSelector((store) => store.cart);
+	return (
+		<div className={styles.cardBlock}>
+			<Link to={`/product/${id}`}>
+				<img src={`http://localhost:3333${image}`} alt={title} />
 
-  const dispatch = useDispatch();
-  const product = {
-    id: id,
-    title: title,
-    image: image,
-    price: price,
-    discont_price: discont_price,
-  };
-  const handleAddToCart = (event) => {
-    // event.preventDefault();
-    console.log(product);
-    dispatch(addProduct(product));
-    dispatch(countTotalSum());
-    console.log(productsInCart);
-  };
+				<div className={styles.descriptionBlock}>
+					<p>{title}</p>
+					<div className={styles.priceBlock}>
+						<p>
+							{"\u0024"}
+							{discont_price === null
+								? price.toFixed()
+								: discont_price.toFixed()}
+						</p>
+						{discont_price ? (
+							<span>
+								{"\u0024"}
+								{price.toFixed()}
+							</span>
+						) : null}
+					</div>
+				</div>
 
-  return (
-    <div className={styles.cardBlock}>
-      <Link to={`/product/${id}`}>
-        <img src={`http://localhost:3333${image}`} alt={title} />
+				{/* Icons Block */}
+				<div className={styles.cartBlock}>
+					<Link onClick={() => setHeartActive(!heartActive)}>
+						{heartActive ? (
+							<IconHertActive className={styles.iconHeart} size='48' />
+						) : (
+							<IconHeart
+								className={[styles.icon, styles.iconHeart].join(" ")}
+								size='48'
+							/>
+						)}
+					</Link>
+					<Link
+						onClick={event => {
+							event.preventDefault();
+							setBagActive(!bagActive);
+							handleAddToCart();
+						}}
+					>
+						{bagActive ? (
+							<IconBagActive className={styles.icon} size='48' />
+						) : (
+							<IconBag className={styles.icon} size='48' />
+						)}
+					</Link>
+				</div>
 
-        <div className={styles.descriptionBlock}>
-          <p>{title}</p>
-          <div className={styles.priceBlock}>
-            <p>
-              {"\u0024"}
-              {price}
-            </p>
-            {discont_price ? (
-              <span>
-                {"\u0024"}
-                {discont_price}
-              </span>
-            ) : null}
-          </div>
-        </div>
-
-        {/* Icons Block */}
-        <div className={styles.cartBlock}>
-          <Link onClick={() => setHeartActive(!heartActive)}>
-            {heartActive ? (
-              <IconHertActive className={styles.iconHeart} size="48" />
-            ) : (
-              <IconHeart
-                className={[styles.icon, styles.iconHeart].join(" ")}
-                size="48"
-              />
-            )}
-          </Link>
-          <Link
-            onClick={(event) => {
-              event.preventDefault();
-              setBagActive(!bagActive);
-              handleAddToCart();
-            }}
-          >
-            {bagActive ? (
-              <IconBagActive className={styles.icon} size="48" />
-            ) : (
-              <IconBag className={styles.icon} size="48" />
-            )}
-          </Link>
-        </div>
-
-        {/* Sale Block */}
-        {discont_price && (
-          <p className={styles.discount}>
-            {(((price - discont_price) / price) * 100).toFixed()}%
-          </p>
-        )}
-      </Link>
-    </div>
-  );
+				{/* Sale Block */}
+				{discont_price && (
+					<p className={styles.discount}>
+						{(((price - discont_price) / price) * 100).toFixed()}%
+					</p>
+				)}
+			</Link>
+		</div>
+	);
 }
--- project_garden/src/pages/SingleProductPage/SingleProductPage.jsx
+++ project_garden/src/pages/SingleProductPage/SingleProductPage.jsx
@@ -4,154 +4,183 @@ import styles from "./SingleProductPage.module.css";
 import { Link, useParams } from "react-router-dom";
 import { ReactComponent as IconPlus } from "../../images/Icons/plus.svg";
 import { ReactComponent as IconMinus } from "../../images/Icons/minus.svg";
-import { ReactComponent as Heart } from "../../images/Icons/heart.svg";
+import { ReactComponent as IconHeart } from "../../images/Icons/heart.svg";
+import { ReactComponent as IconHertActive } from "../../images/Icons/heartActive.svg";
 import { fetchSingleProducts } from "../../store/slices/singleProductSlice";
-import { addProduct, countTotalSum, decreaseProduct } from "../../store/slices/cartSlice";
+import {
+	addProduct,
+	countTotalSum,
+	decreaseProduct,
+} from "../../store/slices/cartSlice";
 import { Context } from "../../context";
+import { addFavorite, removeFavorite } from "../../store/slices/favoritesSlice";
 
 export default function SingleProductPage() {
+	const { nightMode } = useContext(Context);
+	const { setModalActive } = useContext(Context);
 
-  const { nightMode } = useContext(Context);
-  const { setModalActive } = useContext(Context);
-  
-  const dispatch = useDispatch();
-  const { productId } = useParams();
-  const singleProductState = useSelector((store) => store.singleProduct);
+	const dispatch = useDispatch();
+	const { productId } = useParams();
+	const singleProductState = useSelector(store => store.singleProduct);
 
-  useEffect(() => {
-    dispatch(fetchSingleProducts(productId));
-  }, [dispatch, productId]);
+	useEffect(() => {
+		dispatch(fetchSingleProducts(productId));
+	}, [dispatch, productId]);
 
-  const { status, product } = singleProductState;
+	const { status, product } = singleProductState;
 
-  // Ensure product is defined and not empty before destructuring
-  const productDetails = product && product.length > 0 ? product[0] : {};
+	// Ensure product is defined and not empty before destructuring
+	const productDetails = product && product.length > 0 ? product[0] : {};
 
-  const { id, title, price, discont_price, description, image } = productDetails;
+	const { id, title, price, discont_price, description, image } =
+		productDetails;
 
-  const [quantity, setQuantity] = useState(1);
+	const { favorites } = useSelector(store => store.favorites);
+	const isProductInFavorite = favorites.some(
+		favoriteProduct => favoriteProduct.id === id
+	);
+	const [heartActive, setHeartActive] = useState(isProductInFavorite);
 
-  const handleIncrement = () => {
-    setQuantity((prevQuantity) => prevQuantity + 1);
-  };
+	useEffect(() => {
+		setHeartActive(isProductInFavorite);
+	}, [isProductInFavorite]);
 
-  const handleDecrement = () => {
-    setQuantity((prevQuantity) => (prevQuantity > 1 ? prevQuantity - 1 : 1));
-  };
+	const handleAddToFavorites = event => {
+		event.preventDefault();
+		if (isProductInFavorite) {
+			dispatch(removeFavorite(productDetails));
+		} else {
+			dispatch(addFavorite(productDetails));
+		}
+	};
 
-  const handleAddToCart = () => {
-    if (productDetails && Object.keys(productDetails).length > 0) {
-      dispatch(addProduct({ ...productDetails, quantity }));
-      dispatch(countTotalSum());
-    }
-  };
+	const [quantity, setQuantity] = useState(1);
 
-  return (
-    <div className="container">
-      {status === "loading" ? (
-        <p>Loading...</p>
-      ) : (
-        <div className={styles.singleProductCard}>
-          <div
-            className={styles.imgBlock}
-            onClick={() => {
-              setModalActive(true);
-            }}
-          >
-            <img src={`http://localhost:3333${image}`} alt={title} />
-          </div>
+	const handleIncrement = () => {
+		setQuantity(prevQuantity => prevQuantity + 1);
+	};
 
-          <div className={styles.infoBlock}>
-            <div className={styles.titleBlock}>
-              <h2
-                className={`${styles.title} ${
-                  nightMode ? styles.night_mode : ""
-                }`}
-              >
-                {title}
-              </h2>
-              <Heart className={`${styles.icon} ${
-                  nightMode ? styles.night_mode : ""
-                }`} />
-            </div>
+	const handleDecrement = () => {
+		setQuantity(prevQuantity => (prevQuantity > 1 ? prevQuantity - 1 : 1));
+	};
 
-            <div className={styles.priceBlock}>
-              <div className={styles.prices}>
-                <p
-                  className={`${styles.price} ${
-                    nightMode ? styles.night_mode : ""
-                  }`}
-                >
-                  {"\u0024"}
-                  {price}
-                </p>
-                {discont_price ? (
-                  <span>
-                    {"\u0024"}
-                    {discont_price}
-                  </span>
-                ) : null}
-              </div>
+	const handleAddToCart = () => {
+		if (productDetails && Object.keys(productDetails).length > 0) {
+			dispatch(addProduct({ ...productDetails, quantity }));
+			dispatch(countTotalSum());
+		}
+	};
 
-              <div>
-                {discont_price && (
-                  <p
-                    className={`${styles.discount} ${
-                      nightMode ? styles.night_mode : ""
-                    }`}
-                  >
-                    {"\u002d"}
-                    {(((price - discont_price) / price) * 100).toFixed()}%
-                  </p>
-                )}
-              </div>
-            </div>
+	return (
+		<div className={styles.wrapper}>
+			<div className='container'>
+				{status === "loading" ? (
+					<p>Loading...</p>
+				) : (
+					<div className={styles.singleProductCard}>
+						<div className={styles.titleBlock}>
+							<h2
+								className={`${styles.title} ${
+									nightMode ? styles.night_mode : ""
+								}`}
+							>
+								{title}
+							</h2>
+							<Link onClick={handleAddToFavorites}>
+								{heartActive ? (
+									<IconHertActive className={styles.iconHeart} />
+								) : (
+									<IconHeart className={styles.iconHeart} />
+								)}
+							</Link>
+						</div>
 
-            <div className={styles.cartBlock}>
-              <div className={styles.countBlock}>
-                <Link onClick={handleDecrement}>
-                  <IconMinus />
-                </Link>
-                <p
-                  className={`${styles.count} ${
-                    nightMode ? styles.night_mode : ""
-                  }`}
-                >
-                  {quantity}
-                </p>{" "}
-                <Link onClick={handleIncrement}>
-                  <IconPlus />
-                </Link>
-              </div>
+						<div
+							className={styles.imgBlock}
+							onClick={() => {
+								setModalActive(true);
+							}}
+						>
+							<img src={`http://localhost:3333${image}`} alt={title} />
 
-              <button onClick={handleAddToCart} className={styles.cartBtn}>
-                Add to cart
-              </button>
-            </div>
+							{discont_price && (
+								<p
+									className={`${styles.discount} ${
+										nightMode ? styles.night_mode : ""
+									}`}
+								>
+									{"\u002d"}
+									{(((price - discont_price) / price) * 100).toFixed()}%
+								</p>
+							)}
+						</div>
 
-            <div
-              className={`${styles.descriptionBlock} ${
-                nightMode ? styles.night_mode : ""
-              }`}
-            >
-              <h5
-                className={`${styles.descriptionTitle} ${
-                  nightMode ? styles.night_mode : ""
-                }`}
-              >
-                Description
-              </h5>
-              <p
-                className={`${styles.description} ${
-                  nightMode ? styles.night_mode : ""
-                }`}
-              >
-                {description}
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+						<div className={styles.infoBlock}>
+							<div className={styles.priceBlock}>
+								<p
+									className={`${styles.price} ${
+										nightMode ? styles.night_mode : ""
+									}`}
+								>
+									{"\u0024"}
+									{discont_price === null
+										? price.toFixed()
+										: discont_price.toFixed()}
+								</p>
+								{discont_price ? (
+									<span>
+										{"\u0024"}
+										{price.toFixed()}
+									</span>
+								) : null}
+							</div>
+
+							<div className={styles.cartBlock}>
+								<div className={styles.countBlock}>
+									<Link onClick={handleDecrement}>
+										<IconMinus className={styles.iconCount} />
+									</Link>
+									<p
+										className={`${styles.count} ${
+											nightMode ? styles.night_mode : ""
+										}`}
+									>
+										{quantity}
+									</p>{" "}
+									<Link onClick={handleIncrement}>
+										<IconPlus className={styles.iconCount} />
+									</Link>
+								</div>
+							</div>
+
+							<button onClick={handleAddToCart} className={styles.cartBtn}>
+								Add to cart
+							</button>
+						</div>
+
+						<div
+							className={`${styles.descriptionBlock} ${
+								nightMode ? styles.night_mode : ""
+							}`}
+						>
+							<h5
+								className={`${styles.descriptionTitle} ${
+									nightMode ? styles.night_mode : ""
+								}`}
+							>
+								Description
+							</h5>
+							<p
+								className={`${styles.description} ${
+									nightMode ? styles.night_mode : ""
+								}`}
+							>
+								{description}
+							</p>
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
