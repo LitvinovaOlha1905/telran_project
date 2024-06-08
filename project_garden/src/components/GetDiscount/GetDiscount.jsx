59aff37839c50b074613ebe7457631b302b3d336
Olha Litvinova <130159769+LitvinovaOlha1905@users.noreply.github.com>
2024-06-08 14:27:40 +0200

    added adaptiv

--- project_garden/src/components/GetDiscount/GetDiscount.jsx
+++ project_garden/src/components/GetDiscount/GetDiscount.jsx
@@ -5,19 +5,21 @@ import styles from './GetDiscount.module.css';
 
 const GetDiscount = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className="container">
-        <div className={styles.containerForm}>
-          <h2 className={styles.title}>5% off on the first order</h2>
+		<div className={styles.wrapper}>
+			<div className='container'>
+				<div className={styles.formBlock}>
+					<h2 className={styles.title}>5% off on the first order</h2>
 
-          <article className={styles.container_form}>
-            <DiscountForm />
-            <img className={styles.image} src={image} alt="Image" />
-          </article>
-        </div>
-      </div>
-    </div>
-  );
+					<div className={styles.containerForm}>
+							<DiscountForm />
+						<div className={styles.imgBlock}>
+							<img className={styles.image} src={image} alt='Image' />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default GetDiscount;
